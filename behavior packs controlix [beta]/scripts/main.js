4dec523e2362d3498edfc9e7c76ee98008d9af88
Zenn Stecu <afriansah457@gmail.com>
2026-04-19 12:27:30 +0700

    update behavior packs controlix [beta]/scripts/main.js

--- behavior packs controlix [beta]/scripts/main.js
+++ behavior packs controlix [beta]/scripts/main.js
@@ -4,8 +4,15 @@ import { openSmartphoneUI } from "./features/smartphone.js";
 world.afterEvents.itemUse.subscribe((event) => {
   const { source: player, itemStack } = event;
   
-  // Memastikan item yang digunakan adalah smartphone kita
+  // Deteksi jika item adalah Smartphone
   if (itemStack.typeId === "controlix:smartphone_1") {
     openSmartphoneUI(player);
+  } 
+  // Deteksi jika item adalah Land Claim Tool
+  else if (itemStack.typeId === "controlix:land_claim") {
+    // Untuk sementara, kita berikan pesan percobaan
+    player.sendMessage("Memulai proses klaim lahan...");
+    
+    // Nanti kita bisa memanggil fungsi khusus land claim di sini
   }
 });
